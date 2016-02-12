c5bbe03c68b5a2a0488db771dc4b50336a924730
originye <origin.ye@gmail.com>
2016-02-12 15:16:28 +0100

    clean test.py

--- PolicyUpdates/test.py
+++ PolicyUpdates/test.py
@@ -3,31 +3,6 @@ from seq_trie import Seq, SeqTrie
 import multiprocessing as mp
 from multiprocessing import  Manager
 
-# matching_fields_list = ['dl_vlan', 'metadata', 'in_port', 'dl_src', 'dl_dst', 'nw_src', 'nw_dst']
-# #
-# s= ["s1"]
-# push(["s1"] + ["1234"] + ["metadata=1234,in_port=1,ip_src=10.10.10.10"])
-# flow = pull(s)
-# # flow1 = {'cid':'11','pid':'11','dl_vlan':'1111','in_port':'1','metadata':'0x111'}
-# print flow
-# x = Seq()
-# seq_trie = policy_store_to_trie(x, flow)
-# remove(["s1"] + ["1234"])
-# push(["s1"] + ["1222"] + ["metadata=1234,in_port=2,ip_src=10.10.10.10"])
-# flow1 = pull(s)
-# print conflict_detection(seq_trie, flow1)
-# x.update(flow1)
-# print flow1
-# print x.dl_dst
-# seq = SeqTrie(x)
-# print seq.metadata.get_value(u'0x111')
-# # s= ["s1"]
-# # flow = pull(s)
-# # print flow
-# # print flow['cid']
-#print b
-
-#compare_list = ['metadata', 'in_port', 'dl_src', 'dl_dst', 'nw_src', 'nw_dst']
 
 def conflict_test():
     seq = Seq()
@@ -99,19 +74,7 @@ try:
     main_test()
 except ValueError:
     print pull(['s1'])
-#print conflict_test() == True
-# flow_dict = {}
-# items = a.split(",")
-# print items
-# for item in items:
-#     item = item.strip()
-#     print item
-#     if item == '':
-#         continue
-#     key, value = item.split("=")
-#     flow_dict[key] = value
-#
-# print flow_dict
+
 
 
 
